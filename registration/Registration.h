5bdb22ad45c2293f07dee73a39a5dc2a20b2a8ad
Микола Селецький <Seletskyi.Mykola@outlook.com>
2020-07-20 12:55:45 +0300

    ffgga

--- registration/Registration.h
+++ registration/Registration.h
@@ -2,14 +2,65 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <exception>
 #include <string>
+using std::exception;
 using std::string;
 using std::vector;
-using std::search;
+using std::find;
+using std::endl;
+using std::cout;
+class loginAlreadyRegisteredException :public exception
+{
+	string Message;
+public:
+	loginAlreadyRegisteredException(const string& Message):Message(Message)
+	{};
+	char const* what() const override
+	{
+		const_cast<string&>(Message).insert(0, "this login is registered by : ");
+		return Message.c_str();
+	}
+};
+class shortPasswordException :public exception
+{
+	string Message;
+public:
+	shortPasswordException(const string& Message) :Message(Message)
+	{};
+	char const* what() const override
+	{
+		const_cast<string&>(Message).insert(0, "this password is short : ");
+		return Message.c_str();
+	}
+};
+class wrongUserLoginException :public exception
+{
+	string Message;
+public:
+	wrongUserLoginException(const string& Message) :Message(Message)
+	{};
+	char const* what() const override
+	{
+		const_cast<string&>(Message).insert(0, "wrong login or password : ");
+		return Message.c_str();
+	}
+};
 struct loginPasssword
 {
 	string login;
 	string password;
+	bool operator==(const loginPasssword& obj)
+	{
+		if (login==obj.login)
+		{
+			if (password== obj.password)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 };
 class RegistrationAndLogin
 {
@@ -23,24 +74,29 @@ public:
 		{
 			if (user.login==i.login)
 			{
-			//	throw такий користувач вже є
+				throw loginAlreadyRegisteredException(user.login.c_str());//цей логін вже є
 			}
 		}
-		if (user.password.size() <= minSizePass)
+		if (user.password.size() < minSizePass)
 		{
-			//	throw короткий пароль
+			string temp;
+			temp.resize(user.password.size(), '*');
+			throw shortPasswordException(temp);//короткий пароль
 		}
 		users.push_back(user);
+		cout << "you are registered" << endl;
 	}
 	void login(const loginPasssword& user)
 	{
-		if (search(users.begin(), users.end(),user)!=users.end())
+		if (find(users.begin(), users.end(),user) != users.end())
 		{
-			std::cout << "Hello" << std::endl;
+			std::cout << "Hello " << user.login<< std::endl;
 		}
 		else
 		{
-			//	throw неправильний логін або пароль
+			string temp;
+			temp.resize(user.password.size(), '*');
+			throw wrongUserLoginException((user.login + '(' + temp + ')'));// неправильний логін або пароль
 		}
 		
 	}
--- registration/Source.cpp
+++ registration/Source.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include "Registration.h"
 using namespace std;
 int main()
 {
+	RegistrationAndLogin regAndLog;
 
+	try
+	{
+		regAndLog.login({ "User","Password" });//пробуєм залогінитись але користувача немає (ошибка wrongUserLoginException)
+	}
+	catch (const exception& ex)
+	{
+		cout << ex.what()<<endl;
+	}
 
+	try
+	{
+		regAndLog.registration({ "User","Password" });//все ок
+		regAndLog.login({ "User","Password" });//все ок
+	}
+	catch (const exception& ex)
+	{
+		cout << ex.what() << endl;
+	}
 
+	try
+	{
+		regAndLog.registration({ "User","sda" });//логін аже зареєстрований(ошибка loginAlreadyRegisteredException)
+	}
+	catch (const exception& ex)
+	{
+		cout << ex.what() << endl;
+	}
+
+	try
+	{
+		regAndLog.registration({ "User2","Passwor" });//короткий пароль (ошибка shortPasswordException)
+	}
+	catch (const exception& ex)
+	{
+		cout << ex.what() << endl;
+	}
 	return 0;
 }
